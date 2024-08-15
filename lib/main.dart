29b88b1f4917585886373dbf4c573c21b7af7b97
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-15 15:10:09 +0500

    Get All Task Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:b2_backend/views/create_task.dart';
+import 'package:b2_backend/views/get_task.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CreateTaskView(),
+      home: GetAllTaskView(),
     );
   }
 }
---
+++ lib/views/get_task.dart
@@ -0,0 +1,42 @@
+import 'package:b2_backend/models/task.dart';
+import 'package:b2_backend/services/task.dart';
+import 'package:b2_backend/views/create_task.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class GetAllTaskView extends StatelessWidget {
+  const GetAllTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Get All Task"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => CreateTaskView()));
+        },
+        child: Icon(Icons.add),
+      ),
+      body: StreamProvider.value(
+        value: TaskServices().getAllTasks(),
+        initialData: [TaskModel()],
+        builder: (context, child) {
+          List<TaskModel> taskList = context.watch<List<TaskModel>>();
+          return ListView.builder(
+              itemCount: taskList.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  leading: Icon(Icons.task),
+                  title: Text(taskList[i].title.toString()),
+                  subtitle: Text(taskList[i].description.toString()),
+                  trailing: Icon(Icons.delete),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
